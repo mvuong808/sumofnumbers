5f83fd0c0ab2511315aa166606d478792aa10ef9
Mason Vuong <mv.study888@gmail.com>
2026-02-07 15:14:16 -1000

    Created 4 functions for computing sum of numbers

---
+++ sumofnumbers.ts
@@ -0,0 +1,33 @@
+function sumFor(arr : number[]) : number {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+function sumWhile(arr : number[]) : number {
+  let sum = 0;
+  let i = 0;
+  while (i < arr.length) {
+    sum += arr[i];
+    i++;
+  }
+  return sum;
+}
+
+function sumRecursion(arr : number[]) : number {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr[0] + sumRecursion(arr.slice(1));
+}
+
+function sumTheFunctionalWay(arr : number[]) : number {
+  return arr.reduce((acc, val) => acc + val, 0);
+}
+
+console.log(`Sum using for loop: ${sumFor([1, 2, 3, 4])}`);
+console.log(`Sum using while loop: ${sumWhile([1, 2, 3, 4])}`);
+console.log(`Sum using recursion: ${sumRecursion([1, 2, 3, 4])}`);
+console.log(`Sum using functional programming: ${sumTheFunctionalWay([1, 2, 3, 4])}`);
